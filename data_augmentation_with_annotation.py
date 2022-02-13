20f158f64d93eb84dcf3ffbcc5d577989f450b4a
Harsh Murari <hmurari@visionify.ai>
2022-02-13 12:30:36 -0700

    Adjust the transformations to favor motion blur

--- data_augmentation_with_annotation.py
+++ data_augmentation_with_annotation.py
@@ -16,14 +16,14 @@ import xml.etree.ElementTree as ET
 import shutil
 
 AUG_NB_AUGMENTATION_PER_IMAGE = int(
-    os.getenv('AUG_NB_AUGMENTATION_PER_IMAGE', 10))
+    os.getenv('AUG_NB_AUGMENTATION_PER_IMAGE', 1))
 
-DATASET_DIR = "/home/abhian/Pictures/tags/gen2_shelf_icecream"
-DATASET_IMAGES_DIR = "{}/img".format(DATASET_DIR)
-DATASET_ANNOTS_DIR = "{}/annotation".format(DATASET_DIR)
-DATASET_AUGM_DIR = "/home/abhian/Pictures/tags/gen2_shelf_icecream/augmented"
-DATASET_AUGM_IMAGES_DIR = "{}/img".format(DATASET_AUGM_DIR)
-DATASET_AUGM_ANNOTS_DIR = "{}/annotation".format(DATASET_AUGM_DIR)
+DATASET_DIR = "/data/jetson-inference/python/training/detection/ssd/data/bev_test"
+DATASET_IMAGES_DIR = "{}/JPEGImages".format(DATASET_DIR)
+DATASET_ANNOTS_DIR = "{}/Annotations".format(DATASET_DIR)
+DATASET_AUGM_DIR = "/data/jetson-inference/python/training/detection/ssd/data/bev_test_aug"
+DATASET_AUGM_IMAGES_DIR = "{}/JPEGImages".format(DATASET_AUGM_DIR)
+DATASET_AUGM_ANNOTS_DIR = "{}/Annotations".format(DATASET_AUGM_DIR)
 
 def create_directory(path, remove_dir=False):
     if (remove_dir):
@@ -45,7 +45,7 @@ def get_image_annotation_path(stem):
     annotation_path = "{}/{}.xml".format(DATASET_ANNOTS_DIR, stem)
     if (os.path.isfile(annotation_path) == False):
         print("Annotation not found for " + stem)
-        return False
+        return None
     return annotation_path
 
 def decode_image_annotation(path_annotation):
@@ -136,9 +136,9 @@ def get_xml_from_dataframe(origin_annotation_path, labels_df):
         if (isnan(label_df["xmin"]) == False):
             object_et = ET.SubElement(annotation_et, 'object')
             ET.SubElement(object_et, 'name').text = label_df["class"]
-            ET.SubElement(object_et, 'pose').text = "Unspecified"
-            ET.SubElement(object_et, 'truncated').text = "0"
-            ET.SubElement(object_et, 'difficult').text = "0"
+            # ET.SubElement(object_et, 'pose').text = "Unspecified"
+            # ET.SubElement(object_et, 'truncated').text = "0"
+            # ET.SubElement(object_et, 'difficult').text = "0"
             bndbox_et = ET.SubElement(object_et, 'bndbox')
             ET.SubElement(bndbox_et, 'xmin').text = str(
                 floor(label_df["xmin"]))
@@ -163,6 +163,8 @@ for image_path in images_path:
     stem = image_path_data.stem
     basename = image_path_data.name
     image_annotation_path = get_image_annotation_path(stem)
+    if image_annotation_path is None:
+        continue
     image_annotation_path_data = Path(image_annotation_path)
 
     print("Processing {}...".format(stem))
@@ -174,56 +176,21 @@ for image_path in images_path:
     shutil.copy(image_annotation_path,
                 "{}/{}".format(DATASET_AUGM_ANNOTS_DIR, image_annotation_path_data.name))
     for i in range(0, AUG_NB_AUGMENTATION_PER_IMAGE):
-        # This setup of augmentation parameters will pick 1 to 4
+        # This setup of augmentation parameters will pick 1 to 2
         # of the given augmenters and apply them in random order.
         def sometimes(aug): return augmenters.Sometimes(0.5, aug)
         aug_config = augmenters.SomeOf(
-            (1, 4),
+            (1, 2),
             [
-                augmenters.Affine(scale=(0.5, 1.5)),
-                augmenters.Affine(rotate=(-60, 60)),
-                augmenters.Affine(translate_percent={
-                                  "x": (-0.3, 0.3), "y": (-0.3, 0.3)}),
-                augmenters.Fliplr(1),
-                sometimes(
-                    augmenters.Superpixels(
-                        p_replace=(0, 1.0),
-                        n_segments=(20, 200)
-                    )
-                ),
+                augmenters.MotionBlur(k=15),
                 augmenters.OneOf([
-                    augmenters.GaussianBlur((0, 3.0)),
-                    augmenters.AverageBlur(k=(2, 7)),
-                    augmenters.MedianBlur(k=(3, 11)),
-                ]),
-                augmenters.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),
-                augmenters.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),
-                sometimes(augmenters.OneOf([
-                    augmenters.EdgeDetect(alpha=(0, 0.7)),
-                    augmenters.DirectedEdgeDetect(
-                        alpha=(0, 0.7), direction=(0.0, 1.0)
-                    ),
-                ])),
-                augmenters.AdditiveGaussianNoise(
-                    loc=0, scale=(0.0, 0.05*255), per_channel=0.5
-                ),
-                augmenters.OneOf([
-                    augmenters.Dropout((0.01, 0.1), per_channel=0.5),
-                    augmenters.CoarseDropout(
-                        (0.03, 0.15), size_percent=(0.02, 0.05),
-                        per_channel=0.2
-                    ),
-                ]),
-                augmenters.Invert(0.05, per_channel=True),
-                augmenters.Add((-10, 10), per_channel=0.5),
-                augmenters.Multiply((0.5, 1.5), per_channel=0.5),
-                augmenters.ContrastNormalization((0.5, 2.0), per_channel=0.5),
-                augmenters.Grayscale(alpha=(0.0, 1.0)),
-                sometimes(
-                    augmenters.ElasticTransformation(
-                        alpha=(0.5, 3.5), sigma=0.25)
-                ),
-                sometimes(augmenters.PiecewiseAffine(scale=(0.01, 0.05)))
+                    augmenters.Affine(scale=(0.9, 1.2)),
+                    augmenters.Affine(rotate=(-5, 5)),
+                    augmenters.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),
+                    augmenters.Add((-10, 10), per_channel=0.5),
+                    augmenters.Grayscale(alpha=(0.0, 1.0)),
+                    augmenters.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25),
+                    ]),
             ],
             random_order=True
         )
